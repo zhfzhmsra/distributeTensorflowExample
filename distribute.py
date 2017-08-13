27eb04596436b0890d80d34ef10cb730dab52643
zhfzhmsra <zhfzhmsra@gmail.com>
2017-08-13 13:25:23 +0800

    s

--- distribute.py
+++ distribute.py
@@ -21,6 +21,21 @@ tf.app.flags.DEFINE_integer("issync", 0, "是否采用分布式的同步模式�
 learning_rate = FLAGS.learning_rate
 steps_to_validate = FLAGS.steps_to_validate
 
+PS_NUM = len(FLAGS.ps_hosts.split(","))
+WORKER_NUM = len(FLAGS.worker_hosts.split(","))
+
+def create_done_queue(i):
+  """Queue used to signal death for i'th ps shard. Intended to have 
+  all workers enqueue an item onto it to signal doneness."""
+
+  with tf.device("/job:ps/task:%d" % (i)):
+    return tf.FIFOQueue(WORKER_NUM, tf.int32, shared_name="done_queue" +
+                                                             str(i))
+
+
+def create_done_queues():
+  return [create_done_queue(i) for i in range(PS_NUM)]
+
 def main(_):
   ps_hosts = FLAGS.ps_hosts.split(",")
   worker_hosts = FLAGS.worker_hosts.split(",")
@@ -29,7 +44,17 @@ def main(_):
 
   issync = FLAGS.issync
   if FLAGS.job_name == "ps":
-    server.join()
+    #server.join()
+    sess = tf.Session(server.target)
+    queue = create_done_queue(FLAGS.task_index)
+
+    # wait until all workers are done
+    for i in range(WORKER_NUM):
+      sess.run(queue.dequeue())
+      print("ps %d received done %d" % (FLAGS.task_index, i))
+
+    print("ps %d: quitting" % (FLAGS.task_index))
+
   elif FLAGS.job_name == "worker":
     with tf.device(tf.train.replica_device_setter(
                     worker_device="/job:worker/task:%d" % FLAGS.task_index,
@@ -65,12 +90,18 @@ def main(_):
         train_op = optimizer.apply_gradients(grads_and_vars,
                                        global_step=global_step)
 
-
-      init_op = tf.initialize_all_variables()
+      # if issync:
+      #init_op = tf.initialize_all_variables()
+      init_op = tf.global_variables_initializer()
       
       saver = tf.train.Saver()
       tf.summary.scalar('cost', loss_value)
       summary_op = tf.summary.merge_all()
+
+      enq_ops = []
+      for q in create_done_queues():
+        qop = q.enqueue(1)
+        enq_ops.append(qop)
  
     sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
                             logdir="./checkpoint/",
@@ -86,13 +117,16 @@ def main(_):
         sv.start_queue_runners(sess, [chief_queue_runner])
         sess.run(init_token_op)
       step = 0
-      while  step < 1000000:
+      while  step < 100000:
         train_x = np.random.randn(1)
         train_y = 2 * train_x + np.random.randn(1) * 0.33  + 10
         _, loss_v, step = sess.run([train_op, loss_value,global_step], feed_dict={input:train_x, label:train_y})
         if step % steps_to_validate == 0:
           w,b = sess.run([weight,biase])
           print("step: %d, weight: %f, biase: %f, loss: %f" %(step, w, b, loss_v))
+
+      for op in enq_ops:
+        sess.run(op)
 
     sv.stop()
 
@@ -102,7 +136,12 @@ def loss(label, pred):
 
 
 if __name__ == "__main__":
-  tf.app.run()
+  try:
+    tf.app.run()
+  except Exception as e:
+    print ('error occured: {}'.format(e))
+  finally:
+    print ('Finished')
 
 
 
